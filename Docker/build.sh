a8dea40065ce11ae3837e07dfc705de24099a46c
Leonana69 <chengj0803@gmail.com>
2024-04-14 02:03:28 -0400

    update

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,7 +1,24 @@
-# remove old container and image
+# UI permisions
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+xhost +local:docker
+
+
 docker stop -t 0 typefly-ros
 docker image rm -f typefly-ros:0.1
 docker rm -f typefly-ros &>/dev/null
 
 # build
 docker build -t typefly-ros:0.1 .
+docker run -td --privileged --net=host --ipc=host \
+    --name="typefly-ros" \
+    -e "DISPLAY=$DISPLAY" \
+    -e "QT_X11_NO_MITSHM=1" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -e "XAUTHORITY=$XAUTH" \
+    --cap-add=SYS_PTRACE \
+    -v /etc/group:/etc/group:ro \
+    typefly-ros:0.1
--- Docker/run.sh
+++
@@ -1,6 +0,0 @@
-# create a new container
-docker run -td --privileged --net=host --ipc=host \
-    --name="typefly-ros" \
-    -e "DISPLAY=$DISPLAY" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    typefly-ros:0.1
