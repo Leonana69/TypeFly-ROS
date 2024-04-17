c3326f328fe4f785804a0ac2ebe3455e9659733f
Leonana69 <chengj0803@gmail.com>
2024-04-16 22:41:46 -0400

    update

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,7 +3,6 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
 xhost +local:docker
 
 docker stop -t 0 typefly-ros
